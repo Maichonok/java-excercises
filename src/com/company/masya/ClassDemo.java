1f77d12182da08d0cede069e9c44d1460278356d
Mayya Matueva <masya@kurumkans-MacBook-Air.local>
2019-05-16 14:36:42 +0200

    add file ClassDemo

---
+++ src/com/company/masya/ClassDemo.java
@@ -0,0 +1,25 @@
+package com.company.masya;
+
+import javax.swing.*;
+    class MyClass {
+    int num;
+    char symb;
+    String text;
+}
+// Главный класс программы:
+    public class ClassDemo{
+    public static void main(String[] args){
+        MyClass obj=new MyClass();
+        // Заполнение полей объекта:
+        obj.num=12;
+        obj.symb='A';
+        obj.text="Текстовое поле";
+        // Текст для отображения в окне сообщения:
+        String str="Значения полей объекта:\n";
+        str+="num="+obj.num+"\n";
+        str+="symb="+obj.symb+"\n";
+        str+="text="+obj.text;
+        // Отображение окна:
+        JOptionPane.showMessageDialog(null,str);}
+}
+
