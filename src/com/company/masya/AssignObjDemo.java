7e1eb3e41a7df49e111bc480de3b05ca28adc73f
Mayya Matueva <masya@kurumkans-MacBook-Air.local>
2019-05-16 15:51:34 +0200

    add file  ClassDemo5

---
+++ src/com/company/masya/AssignObjDemo.java
@@ -0,0 +1,33 @@
+package com.company.masya;
+
+import javax.swing.*;
+class AssignObj{
+    // Текстовое поле класса:
+    String text;
+    // Конструктор класса (без аргумента):
+    AssignObj(){
+        text="Новый объект!";}
+    // Конструктор класса (с одним аргументом):
+    AssignObj(String str){
+        text=str;}
+    // Метод для отображения текста:
+    void show(){
+        JOptionPane.showMessageDialog(null,text);}
+}
+public class AssignObjDemo{
+    public static void main(String[] args){
+        // Создание объектов:
+        AssignObj objA=new AssignObj();
+        AssignObj objB=new AssignObj("Второй объект!");
+        // Отображение текстовых полей объектов:
+        objA.show();
+        objB.show();
+        // Присваивание объектов:
+        objA=objB;
+        // Текстовое поле первого объекта:
+        objA.show();
+        // Изменение поля второго объекта:
+        objB.text="Изменен второй объект!";
+        // Текстовое поле первого объекта:
+        objA.show();}
+}
