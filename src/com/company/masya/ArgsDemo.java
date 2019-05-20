4b964474e53409cf655d2a9e53ebc6ecc353f7d3
Maya <36052921+Maichonok@users.noreply.github.com>
2019-05-20 13:17:24 +0200

    change file

--- src/com/company/masya/ArgsDemo.java
+++ src/com/company/masya/ArgsDemo.java
@@ -14,28 +14,34 @@ System.out.println("Поля объекта: "+number+" и "+symb);}
 public class ArgsDemo{
     // Перегруженный статический метод. // Аргумент - объект:
     static void MakeChange(MyClass6 obj){
-// Изменение полей объекта - аргумента метода: obj.number++;
+// Изменение полей объекта - аргумента метода: 
+        obj.number++;
         obj.symb++;
 // Текст для отображения:
-        String text="Аргумент-объект: поля "+obj.number+" и "+obj.symb; // Вывод сообщения на консоль:
+        String text="Аргумент-объект: поля "+obj.number+" и "+obj.symb; 
+        // Вывод сообщения на консоль:
         System.out.println(text);}
 // Перегруженный статический метод.
 // Аргументы - переменные базовых типов:
 static void MakeChange(int number,char symb){
-// Изменение аргументов метода: number++;
+// Изменение аргументов метода: 
+    number++;
     symb++;
     // Текст для отображения:
-    String text="Аргументы базовых типов: значения "+number+" и "+symb; // Вывод сообщения на консоль:
+    String text="Аргументы базовых типов: значения "+number+" и "+symb; 
+    // Вывод сообщения на консоль:
 System.out.println(text);}
     // Главный метод программы:
     public static void main(String[] args){
 // Создание объекта:
-        MyClass6 obj=new MyClass6(1,'a'); // Значения полей объекта: obj.show();
+        MyClass6 obj=new MyClass6(1,'a'); // Значения полей объекта: 
+        obj.show();
 // Изменение объекта:
 MakeChange(obj);
 // Значения полей объекта:
 obj.show();
 // Изменение полей объекта:
-MakeChange(obj.number,obj.symb); // Значения полей объекта:
+MakeChange(obj.number,obj.symb); 
+// Значения полей объекта:
 obj.show();}
     }
