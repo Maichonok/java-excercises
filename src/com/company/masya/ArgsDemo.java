596707eef0474d3efb5cdd3c56626a5a9b7b6fc6
Mayya Matueva <masya@kurumkans-MacBook-Air.local>
2019-05-20 13:13:22 +0200

    change file ArgsDemo and add file Masya1

--- src/com/company/masya/ArgsDemo.java
+++ src/com/company/masya/ArgsDemo.java
@@ -1,41 +1,49 @@
-package com.company.masya;
-class MyClass6{ // Поля класса:
+    package com.company.masya;
+    class MyClass6{ // Поля класса:
     int number;
     char symb;
-// Конструктор класса:
-MyClass6(int number,char symb){
-// Для различения полей и аргументов используем // ключевое слово this:
-this.number=number;
-this.symb=symb;}
-// Метод для отображения значений полей объекта:
-void show(){
-System.out.println("Поля объекта: "+number+" и "+symb);}
-        }
-public class ArgsDemo{
-    // Перегруженный статический метод. // Аргумент - объект:
+    // Конструктор класса:
+    MyClass6(int number,char symb){
+    // Для различения полей и аргументов используем // ключевое слово this:
+    this.number=number;
+    this.symb=symb;}
+    // Метод для отображения значений полей объекта:
+    void show(){
+    System.out.println("Поля объекта: "+number+" и "+symb);}
+    }
+    public class ArgsDemo{
+    // Перегруженный статический метод.
+    // Аргумент - объект:
     static void MakeChange(MyClass6 obj){
-// Изменение полей объекта - аргумента метода: obj.number++;
-        obj.symb++;
-// Текст для отображения:
-        String text="Аргумент-объект: поля "+obj.number+" и "+obj.symb; // Вывод сообщения на консоль:
-        System.out.println(text);}
-// Перегруженный статический метод.
-// Аргументы - переменные базовых типов:
-static void MakeChange(int number,char symb){
-// Изменение аргументов метода: number++;
+    // Изменение полей объекта - аргумента метода:
+    obj.number++;
+    obj.symb++;
+    // Текст для отображения:
+    String text="Аргумент-объект: поля "+obj.number+" и "+obj.symb;
+    // Вывод сообщения на консоль:
+    System.out.println(text);}
+    // Перегруженный статический метод.
+    // Аргументы - переменные базовых типов:
+    static void MakeChange(int number,char symb){
+    // Изменение аргументов метода:
+    number++;
     symb++;
     // Текст для отображения:
-    String text="Аргументы базовых типов: значения "+number+" и "+symb; // Вывод сообщения на консоль:
-System.out.println(text);}
+    String text="Аргументы базовых типов: значения "+number+" и "+symb;
+    // Вывод сообщения на консоль:
+    System.out.println(text);}
     // Главный метод программы:
     public static void main(String[] args){
-// Создание объекта:
-        MyClass6 obj=new MyClass6(1,'a'); // Значения полей объекта: obj.show();
-// Изменение объекта:
-MakeChange(obj);
-// Значения полей объекта:
-obj.show();
-// Изменение полей объекта:
-MakeChange(obj.number,obj.symb); // Значения полей объекта:
-obj.show();}
+    // Создание объекта:
+    MyClass6 obj=new MyClass6(1,'a');
+    // Значения полей объекта:
+    obj.show();
+    // Изменение объекта:
+    MakeChange(obj);
+    // Значения полей объекта:
+    obj.show();
+    // Изменение полей объекта:
+    MakeChange(obj.number,obj.symb);
+    // Значения полей объекта:
+    obj.show();}
     }
---
+++ src/com/company/masya/Masya1.java
@@ -0,0 +1,24 @@
+package com.company.masya;
+
+
+public class Masya1 {
+
+//Необходимо вывести на экран вот такую последовательность чисел:
+//100 90 80 70 60 50 40 30 20 10
+
+    public static void main(String[] args) {
+
+        int i = 100;
+
+
+        while (i >= 10) {
+
+            System.out.print(i + " ");
+
+            i = i - 10;
+        }
+    }
+}
+
+
+
