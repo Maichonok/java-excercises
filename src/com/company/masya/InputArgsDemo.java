2f66a555b22ac7d4e932a925db7d41bb1f91da02
Mayya Matueva <masya@kurumkans-MacBook-Air.local>
2019-06-03 21:35:33 +0200

    add file InputArgsDemo

--- src/com/company/masya/InputArgsDemo.java
+++ src/com/company/masya/InputArgsDemo.java
@@ -1,4 +1,41 @@
 package com.company.masya;
 
-public class InputArgsDemo {
+import javax.swing.*;
+// Класс для записи персональных данных:
+class Fellow{
+    // Имя:
+    String name;
+    // Фамилия:
+    String surname;
+    // Возраст:
+    int age;
+    // Вес (в килограммах):
+    double weight;
+    // Конструктор класса:
+    Fellow(String name,String surname,int age,double weight){
+        this.name=name;
+        this.surname=surname;
+        this.age=age;
+        this.weight=weight;
+        show();}
+    // Метод для отображения сообщения:
+    void show(){
+        String str="Name: "+name+"\n";
+        str+="Surname: "+surname+"\n";
+        str+="Age: "+age+" years\n";
+        str+="Weight: "+weight+" kg";
+        JOptionPane.showMessageDialog(null,str);
+    }
 }
+public class InputArgsDemo {
+    public static void main(String[] args){
+        // "Считывание" параметров командной строки:
+        String name=args[0];
+        String surname=args[1];
+        int age=Integer.parseInt(args[2]);
+        double weight=Double.parseDouble(args[3]);
+        // Создание анонимного объекта класса Fellow:
+        new Fellow(name,surname,age,weight);
+    }
+}
+
